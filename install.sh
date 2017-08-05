270a0b02d33d83b42e693628b9cba95a827e612b
Yordan Petrov <dakatapetrov@gmail.com>
2017-08-06 01:49:12 +0300

    Setup services in a temp directory

--- install.sh
+++ install.sh
@@ -8,16 +8,23 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
+TMP_DIR=$DIR/tmp
+SERVICE_DIR=$DIR/systemd
 SYSTEMD_SERVICE_PATH=/etc/systemd/system/
 
-sed -i "7i\Environment=DIR=$DIR" $DIR/systemd/reset-user-pins@.service
-sed -i "9i\Environment=DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS" $DIR/systemd/stop-pulse@.service
-sed -i "6i\Environment=DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS" $DIR/systemd/start-pulse@.service
+mkdir $TMP_DIR
+cp $SERVICE_DIR/* $TMP_DIR/
 
-sudo cp $DIR/systemd/* $SYSTEMD_SERVICE_PATH
+
+sed -i "7i\Environment=DIR=$DIR" $TMP_DIR/reset-user-pins@.service
+sed -i "9i\Environment=DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS" $TMP_DIR/stop-pulse@.service
+sed -i "6i\Environment=DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS" $TMP_DIR/start-pulse@.service
+
+sudo cp $TMP_DIR/* $SYSTEMD_SERVICE_PATH
 
 sudo systemctl daemon-reload
 sudo systemctl enable stop-pulse@$USER.service
 sudo systemctl enable reset-user-pins@$USER.service
 sudo systemctl enable start-pulse@$USER.service
+
+rm -r $TMP_DIR

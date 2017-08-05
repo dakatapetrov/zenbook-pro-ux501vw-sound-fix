ad78da505802718d1c5abf567181d432a69eb01a
Yordan Petrov <dakatapetrov@gmail.com>
2017-08-06 01:35:03 +0300

    Remove obsolete code

--- reconfig.sh
+++ reconfig.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# credits: https://stackoverflow.com/a/246128/4649594
-# SOURCE="${BASH_SOURCE[0]}"
-# while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-#   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-#   SOURCE="$(readlink "$SOURCE")"
-#   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-# done
-# DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
 USER_PIN_PATH=$1
 
 
